0d857f18125d4f429eba93556ebaafd8bf1bcfb2
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-06 14:23:24 -0500

    Basic instance uniqueness/persistence unit tests

--- Tests/BasicTests.cs
+++ Tests/BasicTests.cs
@@ -16,5 +16,42 @@ namespace Tests
 
             Assert.AreEqual("42", stringDictionary["answer"]);
         }
+
+        [TestMethod]
+        public void MultipleValues()
+        {
+            var stringDictionary = new RDictionary<string, string>("stringDictionary");
+
+            stringDictionary["correct"] = "42";
+            stringDictionary["incorrect"] = "43";
+
+            Assert.AreEqual("42", stringDictionary["correct"]);
+            Assert.AreNotEqual("42", stringDictionary["incorrect"]);
+        }
+
+        [TestMethod]
+        public void MultipleInstancesDifferentKeys()
+        {
+            var s1 = new RDictionary<string, string>("s1");
+            var s2 = new RDictionary<string, string>("s2");
+
+            s1["name"] = "s1";
+            s2["name"] = "s2";
+
+            Assert.AreNotEqual(s1["name"], s2["name"]);
+            Assert.AreEqual("s1", s1["name"]);
+            Assert.AreEqual("s2", s2["name"]);
+        }
+
+        [TestMethod]
+        public void MultipleInstancesSameKey()
+        {
+            var s1 = new RDictionary<string, string>("s1");
+            var s2 = new RDictionary<string, string>("s1");
+
+            s1["name"] = "s1";
+
+            Assert.AreEqual(s1["name"], s2["name"]);
+        }
     }
 }
